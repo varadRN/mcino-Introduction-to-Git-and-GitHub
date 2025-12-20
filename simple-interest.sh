64b815f4365abd766d9aa9cf24f63e1e702784b8
varadRN <varadnakhate289@gmail.com>
2025-12-20 09:14:51 -0500

    Revert "Update simple-interest.sh"
    This reverts commit f111812932f699a3574733d28606f5efefbd47b9.

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+# This script calculates simple interest given principal, annual rate of interest and time period in years.
+# Do not use this in production. Sample purpose only.
 
-# Simple Interest Calculator
+# Author: Upkar Lidder (IBM)
+# Addtional Authors:
+# <your Github username>
 
-echo "Enter the principal amount:"
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
+
+# Output:
+# simple interest = p*t*r
+
+echo "Enter the principal:"
 read p
-
-echo "Enter the annual rate of interest:"
+echo "Enter rate of interest per year:"
 read r
-
-echo "Enter the time period in years:"
+echo "Enter time period in years:"
 read t
 
-simple_interest=$(( (p * r * t) / 100 ))
-
-echo "The simple interest is: $simple_interest"
+s=$(expr $p \* $t \* $r / 100)
+echo "The simple interest is: "
+echo $s
